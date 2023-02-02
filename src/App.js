7bb98f8e8ff20c7eb32ca9ab6770ac46ab161667
Abu Sayeed <sayeed@pop-os.localdomain>
2023-02-02 10:46:57 +0600

    installed tailwind css and daisyui

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <h5>this is a test</h5>
+      <button className='btn btn-primary'>Test</button>
     </div>
   );
 }
